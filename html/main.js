a618a503378863b459ee6261146d71d47166548f
bkcelebi <burakkcelebi@hotmail.com>
2022-05-03 21:58:30 +0100

    Js table population is fixed

--- html/main.js
+++ html/main.js
@@ -34,7 +34,7 @@ let loadData = () => {
                             temp += "<td>"+x.ID+"</td>";
                             temp += "<td>"+x.Name+"</td>";
                             temp += "<td>"+x.Email+"</td>";
-                            temp += "<td>"+"<a href='#'> Update </a><br><a href='#'></a>" +"</td></tr>"
+                            temp += "<td>"+"<a href='#'> Update </a><br><a href='#'> Delete </a>" +"</td></tr>"
                         })
 
                         console.log(temp)
