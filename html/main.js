75ff0733b0de406101dcba31c507276ebcfe9321
bkcelebi <burakkcelebi@hotmail.com>
2022-05-04 11:35:30 +0100

    trying the functionality of the anchor tags

--- html/main.js
+++ html/main.js
@@ -34,7 +34,7 @@ let loadData = () => {
                             temp += "<td>"+x.ID+"</td>";
                             temp += "<td>"+x.Name+"</td>";
                             temp += "<td>"+x.Email+"</td>";
-                            temp += "<td>"+"<a onClick='deleteStudent()'> Update </a><br><a href='#'> Delete </a>" +"</td></tr>;"
+                            temp += "<td>"+"<a onClick='deleteStudent()'> Update </a><br><a> Delete </a>" +"</td></tr>;"
                         })
 
                         console.log(temp)
