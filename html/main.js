4cb4d64af22b23401ba2017aad23457b6edeb50a
bkcelebi <burakkcelebi@hotmail.com>
2022-05-05 17:52:28 +0100

    trying the functionality of the anchor tags

--- html/main.js
+++ html/main.js
@@ -35,7 +35,7 @@ let loadData = () => {
                             temp += "<td>"+x.Name+"</td>";
                             temp += "<td>"+x.Email+"</td>";
                             temp += "<td>"+"<span style='margin-right: 10px;' class='btn btn-outline-warning btn-sm'> Update </span>" + 
-                            '<span class= "btn btn-outline-danger btn-sm"> Delete </span>' +
+                            "<span class='btn btn-outline-danger btn-sm' onclick='return deleteStudent(${x.ID})'> Delete </span>" +
                             "</td></tr>"
                             // onclick="return deleteStudent("$x.ID")"
                         })
@@ -52,23 +52,23 @@ let loadData = () => {
 window.onload = loadData;
  
 
-let deleteStudent = () => {   
-    let deleteEntr = document.getElementById('deleteEnt').value;   
-    fetch(baseURL+'delete?id='+deleteEntr)
-    .then((resp) => {
-        alert("Student Deleted");
-        location.reload();
-    });
-}
-
-// let deleteStudent = (id) => {   
-//     // let deleteEntr = document.getElementById('deleteEnt').value;   
-//     fetch(baseURL+'delete?id='+id)
+// let deleteStudent = () => {   
+//     let deleteEntr = document.getElementById('deleteEnt').value;   
+//     fetch(baseURL+'delete?id='+deleteEntr)
 //     .then((resp) => {
 //         alert("Student Deleted");
 //         location.reload();
 //     });
 // }
+
+let deleteStudent = (id) => {   
+    // let deleteEntr = document.getElementById('deleteEnt').value;   
+    fetch(baseURL+'delete?id='+id)
+    .then((resp) => {
+        alert("Student Deleted");
+        location.reload();
+    });
+}
 
 let updateStudent = () => {
     let updateID = document.getElementById('updateID').value;
