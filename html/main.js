94db9f0cadd34f941af36d3b7764ec93cdbcf6e6
bkcelebi <burakkcelebi@hotmail.com>
2022-05-05 17:55:16 +0100

    trying the functionality of the anchor tags

--- html/main.js
+++ html/main.js
@@ -34,8 +34,8 @@ let loadData = () => {
                             temp += "<td>"+x.ID+"</td>";
                             temp += "<td>"+x.Name+"</td>";
                             temp += "<td>"+x.Email+"</td>";
-                            temp += "<td>"+"<span style='margin-right: 10px;' class='btn btn-outline-warning btn-sm'> Update </span>" + 
-                            "<span class='btn btn-outline-danger btn-sm' onclick='deleteStudent('${x.ID}')'> Delete </span>" +
+                            temp += "<td>"+"<a style='margin-right: 10px;' class='btn btn-outline-warning btn-sm'> Update </a>" + 
+                            "<a class='btn btn-outline-danger btn-sm' onclick='deleteStudent(${x.ID})'> Delete </a>" +
                             "</td></tr>"
                             // onclick="deleteStudent("$x.ID")"
                         })
